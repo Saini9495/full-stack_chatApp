7cb95c12175e1c1b8d7c003aee05d8686a126c2a
rishabh <sainirishabh394@gmail.com>
2026-07-30 14:13:32 +0530

    created build of frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Networking-Detailed-Notes/' ,
+  base: '/full-stack_chatApp/' ,
   plugins: [react()],
 })
